666594fa6c2ad76b46270f0280fbb3bd1f9ce7b6
ghassan-amreyah <1191704@student.birzeit.edu>
2022-12-15 10:32:29 +0200

    version1

---
+++ app/src/main/java/com/example/masroofe/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.masroofe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
+    }
+}
