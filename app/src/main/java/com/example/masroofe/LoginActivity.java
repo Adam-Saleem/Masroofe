5187a40535ee91dba2ad28c695eb213c38e0bf95
ghassan-amreyah <1191704@student.birzeit.edu>
2022-12-17 14:27:27 +0200

    Signup and active buttons

--- app/src/main/java/com/example/masroofe/LoginActivity.java
+++ app/src/main/java/com/example/masroofe/LoginActivity.java
@@ -2,17 +2,54 @@ package com.example.masroofe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn, signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.login_activity);
         getSupportActionBar().hide();
+        setupViews();
+
+        loginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent loginInent = new Intent(LoginActivity.this , MainActivity.class);
+                startActivity(loginInent);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent loginInent = new Intent(LoginActivity.this , SignupActivity.class);
+                startActivity(loginInent);
+            }
+        });
+
 
     }
+
+    //References
+    private void setupViews()
+    {
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtnInLoginActivity);
+    }
+
+
+
+
 }
