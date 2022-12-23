3aa2b2b3adde948ea03ca280b79d78c6682d7fa5
ghassan-amreyah <1191704@student.birzeit.edu>
2022-12-23 14:39:57 +0200

    Add Calendar

--- app/src/main/java/com/example/masroofe/SignupActivity.java
+++ app/src/main/java/com/example/masroofe/SignupActivity.java
@@ -2,16 +2,25 @@ package com.example.masroofe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.util.Calendar;
 
 public class SignupActivity extends AppCompatActivity
 {
 
     private Button finishBtn, nextBtn;
+    private EditText dateBirth;
+    private int day, month, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,6 +29,27 @@ public class SignupActivity extends AppCompatActivity
         setContentView(R.layout.signup_activity);
         setupReference();
         getSupportActionBar().hide();
+
+        Calendar calendarBirthDay = Calendar.getInstance();
+
+        dateBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                year = calendarBirthDay.get(Calendar.YEAR);
+                month = calendarBirthDay.get(Calendar.MONTH);
+                day = calendarBirthDay.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePicker = new DatePickerDialog(SignupActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth)
+                    {
+                        dateBirth.setText(SimpleDateFormat.getDateInstance().format(calendarBirthDay.getTime()));
+                    }
+                }, year, month, day);
+
+                datePicker.show();
+            }
+        });
 
         finishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +75,7 @@ public class SignupActivity extends AppCompatActivity
     {
         finishBtn = findViewById(R.id.finishBtn);
         nextBtn = findViewById(R.id.nextBtn);
+        dateBirth = findViewById(R.id.birthDay);
     }
 
 
