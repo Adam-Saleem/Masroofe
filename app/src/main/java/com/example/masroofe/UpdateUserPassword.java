59ce013f3aaad49dc10c8860319267471a59ce2e
adam <adam@test.com>
2022-12-23 19:43:06 +0200

    Update Menu

--- app/src/main/java/com/example/masroofe/UpdateUserPassword.java
+++ app/src/main/java/com/example/masroofe/UpdateUserPassword.java
@@ -9,7 +9,7 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageVieUw;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class UpdateUserPassword extends AppCompatActivity {
