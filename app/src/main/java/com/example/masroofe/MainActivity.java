2686c223aa8c520513ccd24bf675f7cfe4388a43
adam <adam@test.com>
2022-12-23 18:13:45 +0200

    Clean Imports

--- app/src/main/java/com/example/masroofe/MainActivity.java
+++ app/src/main/java/com/example/masroofe/MainActivity.java
@@ -6,8 +6,6 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -16,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     BottomAppBar appBar;
     MenuItem menuItem;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         getSupportActionBar().hide();
@@ -27,31 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
 
         appBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-                                              @Override
-                                              public boolean onMenuItemClick(MenuItem item) {
-                                                  switch (item.getItemId()) {
-                                                      case R.id.imgHome:
-                                                         Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                                                         startActivity(intent);
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.imgHome:
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
 
-                                                          break;
-                                                      case R.id.imgCal:
-                                                          Toast.makeText(MainActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.imgCal:
+                        Toast.makeText(MainActivity.this, "true", Toast.LENGTH_SHORT).show();
 
-                                                          break;
-                                                      case R.id.imgMenu:
-                                                          Intent intent2 = new Intent(MainActivity.this, SettingsActivity.class);
-                                                          startActivity(intent2);
-                                                          break;
-                                                  }
-                                                  return true;
-                                              }
-                                          });
+                        break;
+                    case R.id.imgMenu:
+                        Intent intent2 = new Intent(MainActivity.this, SettingsActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+                return true;
+            }
+        });
     }
-
-
-
-    }
+}
 
 
 
