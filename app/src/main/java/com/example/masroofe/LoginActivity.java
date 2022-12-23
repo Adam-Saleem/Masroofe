c7e60e67da5bf0109e02a1f3530d44d32183468a
ghassan-amreyah <1191704@student.birzeit.edu>
2022-12-23 18:32:28 +0200

    Login & Signup is done

--- app/src/main/java/com/example/masroofe/LoginActivity.java
+++ app/src/main/java/com/example/masroofe/LoginActivity.java
@@ -3,13 +3,20 @@ package com.example.masroofe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginBtn, signUpBtn;
+    private EditText email, password;
+
+
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +25,65 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setupReference();
 
+        prefs = getSharedPreferences("userInformation",0);
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent loginInent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(loginInent);
+            public void onClick(View view)
+            {
+                String userNameLogin = email.getText().toString();
+                String passwordLogin = password.getText().toString();
+                checkLogin(userNameLogin, passwordLogin);
             }
         });
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent loginInent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(loginInent);
+            public void onClick(View view)
+            {
+                Intent signup = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(signup);
             }
         });
 
 
     }
 
+    //--------------Methods---------------------------------------------------------
     //References
-    private void setupReference() {
+    private void setupReference()
+    {
+        email = findViewById(R.id.emailLogin);
+        password = findViewById(R.id.passwordLogin);
         loginBtn = findViewById(R.id.loginBtn);
         signUpBtn = findViewById(R.id.signUpBtnInLoginActivity);
+    }
+
+    //check login method
+    private void checkLogin(String username, String password)
+    {
+
+        if(username.length() > 1 && password.length() > 1)
+        {
+            //get data from prefs register
+            String regUsername = prefs.getString("username", "");
+            String regPassword = prefs.getString("password", "");
+
+            if(username.trim().equals(regUsername.trim()) && password.trim().equals(regPassword.trim()))
+            {
+                Intent login = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(login);
+                Toast.makeText(LoginActivity.this, "تم الدخول بنجاح", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(LoginActivity.this, "البيانات غير صحيحة! حاول مرة أخرى", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(LoginActivity.this, "أملئ جميع الحقول، وحاول مرة أخرى", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/masroofe/SignupActivity.java
+++ app/src/main/java/com/example/masroofe/SignupActivity.java
@@ -1,36 +1,39 @@
 package com.example.masroofe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.text.SimpleDateFormat;
-import java.time.Year;
 import java.util.Calendar;
 
 public class SignupActivity extends AppCompatActivity
 {
 
     private Button finishBtn, nextBtn;
-    private EditText dateBirth;
+    private EditText fullName, dateBirth, username, password, repeatPassword;
     private int day, month, year;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        //---References-----------------------------
         setupReference();
         getSupportActionBar().hide();
 
         Calendar calendarBirthDay = Calendar.getInstance();
+        prefs = getSharedPreferences("userInformation",0);
+
 
         dateBirth.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +42,7 @@ public class SignupActivity extends AppCompatActivity
                 year = calendarBirthDay.get(Calendar.YEAR);
                 month = calendarBirthDay.get(Calendar.MONTH);
                 day = calendarBirthDay.get(Calendar.DAY_OF_MONTH);
+
                 DatePickerDialog datePicker = new DatePickerDialog(SignupActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth)
@@ -53,29 +57,65 @@ public class SignupActivity extends AppCompatActivity
 
         finishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent finishInent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(finishInent);
+            public void onClick(View view)
+            {
+                String fName = fullName.getText().toString();
+                String bDate = dateBirth.getText().toString();
+                String userName = username.getText().toString();
+                String pass = password.getText().toString();
+                String rePassword = repeatPassword.getText().toString();
 
-                Toast.makeText(SignupActivity.this, "تم إنشاء الحساب", Toast.LENGTH_SHORT).show();
+                //get information about user
+                userData(fName, bDate, userName, pass, rePassword);
             }
         });
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent incomeInent = new Intent(SignupActivity.this, FixedIncomeInfoActivity.class);
-                startActivity(incomeInent);
+            public void onClick(View view)
+            {
+                Intent income = new Intent(SignupActivity.this, FixedIncomeInfoActivity.class);
+                startActivity(income);
             }
         });
     }
 
+    //--------------Methods---------------------------------------------------------
     //References
     private void setupReference()
     {
         finishBtn = findViewById(R.id.finishBtn);
         nextBtn = findViewById(R.id.nextBtn);
         dateBirth = findViewById(R.id.birthDay);
+        fullName = findViewById(R.id.fullName);
+        username = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        repeatPassword = findViewById(R.id.passwordRepeat);
+    }
+
+    //get information about user method
+    private void userData(String fName, String bDate, String username , String password, String rePassword)
+    {
+        if(fName.length() > 1 && bDate.length() > 1 && username.length() > 1 && password.length() > 1 && rePassword.length() > 1)
+        {
+            SharedPreferences.Editor editorPref = prefs.edit();
+            editorPref.putString("fullName", fName);
+            editorPref.putString("dateBirth", bDate);
+            editorPref.putString("username", username);
+            editorPref.putString("password", password);
+            editorPref.putString("repeatPassword", rePassword);
+            editorPref.apply();
+
+            Toast.makeText(SignupActivity.this, "تم إنشاء الحساب", Toast.LENGTH_SHORT).show();
+
+            Intent signup = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(signup);
+        }
+        else
+        {
+            Toast.makeText(SignupActivity.this, "أملئ جميع الحقول، وحاول مرة أخرى", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
