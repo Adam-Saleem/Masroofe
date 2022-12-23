2c86383b92122c89c1243edb370000b09fd16b58
adam <adam@test.com>
2022-12-23 22:44:56 +0200

    add income to main

--- app/src/main/java/com/example/masroofe/MainActivity.java
+++ app/src/main/java/com/example/masroofe/MainActivity.java
@@ -1,27 +1,34 @@
 package com.example.masroofe;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgCal, imgGuide, imgMenu;
+    private ImageView imgCal, imgGuide, imgMenu;
+    private TextView fixedIncome;
+    private SharedPreferences userPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         getSupportActionBar().hide();
+        setUpSharedPrefs();
         setUp();
     }
 
     private void setUp() {
+        String fixed_income = userPrefs.getString("fixedIncome", "");
+        if (fixed_income.length() > 0) {
+            fixedIncome.setText(fixed_income);
+        }
 
-        imgCal = findViewById(R.id.imgCal);
         imgCal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        imgGuide = findViewById(R.id.imgGuide);
         imgGuide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,8 +44,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        imgMenu = findViewById(R.id.imgMenu);
         imgMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,6 +53,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+    private void setUpSharedPrefs() {
+        userPrefs = getSharedPreferences("userInformation", 0);
+        imgCal = findViewById(R.id.imgCal);
+        imgMenu = findViewById(R.id.imgMenu);
+        imgGuide = findViewById(R.id.imgGuide);
+        fixedIncome = findViewById(R.id.fixedIncome);
     }
 }
 
